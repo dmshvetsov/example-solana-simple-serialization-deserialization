e53768fb0d1f053898d081c817e52ba02a6f3593
dima <dima@dimas-MacBook-Pro.local>
2024-01-05 22:18:50 +0200

    add a test file

---
+++ tests/index.ts
@@ -0,0 +1,10 @@
+import { use as chaiUse, expect } from 'chai'
+import chaiAsPromised from 'chai-as-promised'
+
+chaiUse(chaiAsPromised)
+
+describe('fco-game-engine tournament and match store features', () => {
+  it('works', () => {
+    expect(1 + 1).to.equal(2)
+  })
+})
